a4d727264eec7850dcd9ed6e9ece15a7b74d574f
WidBoy <weedboy448@gmail.com>
2019-08-31 21:25:54 +0100

    Commit 1

---
+++ index.js
@@ -0,0 +1,42 @@
+//Definimos el npm
+const Discord = require("discord.js");
+const simplifier = require("./lib/codesimplifier")
+//Creamos el client
+const  client = new Discord.Client();
+
+//Evento de encendido
+client.on("ready", () => {
+   console.log("Estoy listo!");
+});
+
+//Evento de mensaje
+client.on("message", (message) => {
+
+ var prefix = "!";
+
+   if(!message.content.startsWith(prefix)) return;
+   if(message.author.bot) return;
+   const args = message.content.slice(prefix.length).trim().split(/ +/g);
+   const command = args.shift().toLowerCase();
+
+if(command === "ownerid"){
+
+    let owner = simplifier.ownerid(message)
+  message.channel.send(owner)
+
+}
+if(command === "cantidadmiembros"){
+  let cantidad = simplifier.cantidadmiembros(message)
+  message.channel.send(cantidad)
+}
+if(command === "serverid"){
+  let id = simplifier.serverid(message)
+ message.channel.send(id)
+}
+if(command === "servername"){
+  let name = simplifier.servername(message)
+  message.channel.send(name)
+}
+
+});
+client.login("NjE3Mjc3NDA0OTA5NDA0MjAz.XWoyZw.7aN0KJ7yULFTo-q7-eXT5ar1-6Q");
